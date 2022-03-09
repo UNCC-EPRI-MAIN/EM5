9158a39a8c415b1c8ee30797db807dc557ffc762
Jonathon Nguyen <nguyjd@gmail.com>
2022-03-09 16:51:08 -0500

    Added a few test

--- tests/test2/Flags.py
+++ tests/test2/Flags.py
@@ -11,12 +11,12 @@ BatteryMonitor_debug = False
 BatteryMonitor_enabled = False
 
 # Blade control
-BladeControl_debug = True
+BladeControl_debug = False
 BladeControl_enabled = True
 
 # Collision Detection - Bumpers
-CollisionDetection_debug = True
-CollisionDetection_enabled = True
+CollisionDetection_debug = False
+CollisionDetection_enabled = False
 
 # Drive Control
 DriveControl_debug = True
@@ -31,32 +31,54 @@ RemoteControl_debug = True
 RemoteControl_enabled = True
 
 # ---------- Firmware ----------
+# Bumpers
+bumper1_debug = False
+bumper1_enabled = True
+
+bumper2_debug = False
+bumper2_enabled = True
+
+bumper3_debug = False
+bumper3_enabled = False
+
+bumper4_debug = False
+bumper4_enabled = True
+
+bumper5_debug = False
+bumper5_enabled = False
+
+bumper6_debug = False
+bumper6_enabled = False
+
+bumper7_debug = False
+bumper7_enabled = True
+
 # AMT103 - Encoders
 leftEncoder_debug = True
 leftEncoder_enabled = True
 
-rightEncoder_debug = True
-rightEncoder_enabled = True
+rightEncoder_debug = False
+rightEncoder_enabled = False
 
 # MD30C - Blade Motor Driver
-MD30C_debug = True
+MD30C_debug = False
 MD30C_enabled = True
 
 # Relay Controls
-wheelRelay_debug = True
+wheelRelay_debug = False
 wheelRelay_enabled = True
 
-bladeRelay_debug = True
-bladeRelay_enabled = True
+bladeRelay_debug = False
+bladeRelay_enabled = False
 
-chargeRelay_debug = True
-chargeRelay_enabled = True
+chargeRelay_debug = False
+chargeRelay_enabled = False
 
 # Sabertooth2x60 - Wheel Motors
-leftMotor_debug = True
+leftMotor_debug = False
 leftMotor_enabled = True
 
-rightMotor_debug = True
+rightMotor_debug = False
 rightMotor_enabled = True
 
 # Object Detection - LiDar
@@ -64,6 +86,6 @@ rpLiDAR_A2M4_R4_debug = False
 rpLiDAR_A2M4_R4_enabled = False
 
 # GPS 
-NEO_M8P_debug = True
+NEO_M8P_debug = False
 NEO_M8P_enabled = True
 NEO_M8P_RTK_enabled = True
