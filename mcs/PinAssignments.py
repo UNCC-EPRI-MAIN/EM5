d24e46bc6278f6e40863c94a885dd46ee63c11b3
Jonathon Nguyen <nguyjd@gmail.com>
2022-03-09 17:30:08 -0500

    Update PinAssignments.py

--- mcs/PinAssignments.py
+++ mcs/PinAssignments.py
@@ -7,27 +7,18 @@
 # All pin numbers use board Mode mode. See Raspberry Pi GPIO for more information.
 
 # Motor Signals
-bladePWM = 0
-leftMotorPWM = 13
-rightMotorPWM = 12
+bladePWM = 27
+leftMotorPWM = 32
+rightMotorPWM = 33
 
-rtkStatus = 4
+rtkStatus = 37
 
 # Relays
-wheelRelay = 19
-bladeRelay = 6
+wheelRelay = 31
+bladeRelay = 35
 
 # Encoders
-leftEncoderX = 24
-leftEncoderA = 23
-rightEncoderX = 15
-rightEncoderA = 14
-
-# Bumpers
-bumper1 = 25 
-bumper2 = 8
-bumper3 = 7
-bumper4 = 1
-bumper5 = 16
-bumper6 = 20
-bumper7 = 21
+leftEncoderX = 18
+leftEncoderA = 16
+rightEncoderX = 24
+rightEncoderA = 26
