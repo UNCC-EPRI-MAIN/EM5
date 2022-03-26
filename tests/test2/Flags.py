c1ec754594c3dc4d40793cad6f6234f377e71f97
Jonathon Nguyen <nguyjd@gmail.com>
2022-03-26 17:32:18 -0400

    Added a test for GPS

--- tests/test2/Flags.py
+++ tests/test2/Flags.py
@@ -5,6 +5,15 @@
 # Most modules include standard test flags that can be used to facilitate
 # testing and debugging.
 
+# ---------- System Controller ----------
+MowBotControl_debug = True
+
+DriveSysControl_debug = True
+DriveSysControl_enabled = True
+
+InterruptControl_debug = True
+InterruptControl_enabled = True
+
 # ---------- Controllers ----------
 # Battery Monitor
 BatteryMonitor_debug = False
@@ -12,39 +21,43 @@ BatteryMonitor_enabled = False
 
 # Blade control
 BladeControl_debug = False
-BladeControl_enabled = True
+BladeControl_enabled = False
 
-# Collision Detection - Bumpers
+# Collision Detection
 CollisionDetection_debug = False
 CollisionDetection_enabled = False
 
 # Drive Control
-DriveControl_debug = True
-DriveControl_enabled = True
+DriveControl_debug = False
+DriveControl_enabled = False
 
 # Navigation
 Navigation_debug = True
 Navigation_enabled = True
 
 # Remote Control
-RemoteControl_debug = True
-RemoteControl_enabled = True
+RemoteControl_debug = False
+RemoteControl_enabled = False
+
+# Message Handler
+MessageHandler_debug = False
+MessageHandler_enabled = False
 
 # ---------- Firmware ----------
 # AMT103 - Encoders
-leftEncoder_debug = True
-leftEncoder_enabled = True
+leftEncoder_debug = False
+leftEncoder_enabled = False
 
 rightEncoder_debug = False
 rightEncoder_enabled = False
 
 # MD30C - Blade Motor Driver
 MD30C_debug = False
-MD30C_enabled = True
+MD30C_enabled = False
 
 # Relay Controls
 wheelRelay_debug = False
-wheelRelay_enabled = True
+wheelRelay_enabled = False
 
 bladeRelay_debug = False
 bladeRelay_enabled = False
@@ -54,16 +67,24 @@ chargeRelay_enabled = False
 
 # Sabertooth2x60 - Wheel Motors
 leftMotor_debug = False
-leftMotor_enabled = True
+leftMotor_enabled = False
 
 rightMotor_debug = False
-rightMotor_enabled = True
+rightMotor_enabled = False
 
 # Object Detection - LiDar
 rpLiDAR_A2M4_R4_debug = False
 rpLiDAR_A2M4_R4_enabled = False
 
 # GPS 
-NEO_M8P_debug = False
+NEO_M8P_debug = True
 NEO_M8P_enabled = True
-NEO_M8P_RTK_enabled = True
+NEO_M8P_RTK_enabled = False
+
+# Accelerometer
+accelerometer_debug = False
+accelerometer_enabled = False
+
+# UART 
+uart_debug = False
+uart_enabled = False
