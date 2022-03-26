43287214744c9fc90af647e09188a63783835b6b
Jonathon Nguyen <nguyjd@gmail.com>
2022-03-26 18:34:33 -0400

    Update NEO_M9N.py

--- mcs/firmware/NEO_M9N.py
+++ mcs/firmware/NEO_M9N.py
@@ -6,14 +6,13 @@
 
 import serial
 from ublox_gps import UbloxGps
-import RPi.GPIO as GPIO 
 
 # module parameters
 FAILED_READ_ALARM_THRESHOLD = 3
 FAILED_READ_ALARM_FREQUENCY = 10
 
-def run(debug, enabled, rtk_enabled, rtkStatusPin, globals):
-    ## String used for debugging
+def run(debug, enabled, globals):
+    ## Used for debugging
     newLon = -1
     newLat = -1
     oldLon = -1
@@ -26,14 +25,11 @@ def run(debug, enabled, rtk_enabled, rtkStatusPin, globals):
         debugPrefix += "[E]"
     else:
         debugPrefix += "[D]"
-    if rtk_enabled:
-        debugPrefix += "[RTK]"
 
     if enabled:
         try:
             port = serial.Serial('/dev/ttyACM0', baudrate=38400, timeout=1)
             gps = UbloxGps(port)
-            GPIO.setup(rtkStatusPin, GPIO.IN)
             failedReadCount = 0
             while globals['state'] != 'shutdown':
                 try:
@@ -53,23 +49,17 @@ def run(debug, enabled, rtk_enabled, rtkStatusPin, globals):
                     if newHeading != oldHeading and not globals['headingLock']:
                         globals['heading'] = newHeading
 
-                    rtkStatus = GPIO.input(rtkStatusPin)
-                    rtkStatus = not rtkStatus
-
-                    if rtkStatus:
-                        failedReadCount = 0
-
                     if debug:
-                        print(debugPrefix + "[run()]: RTK active = " + str(rtkStatus))
                         print(debugPrefix + "[run()]: X: " + str(geo.lon) + " Y: " + str(geo.lat))
                         print(debugPrefix + "[run()]: heading: " + str(geo.headMot))
+                
                 except (ValueError, IOError) as err:
                     failedReadCount += 1
                     if debug:
                         print(debugPrefix + err)
                         print(debugPrefix + "[run()]: failed read")
                     if failedReadCount >= FAILED_READ_ALARM_THRESHOLD and failedReadCount % 10 == 0:
-                        print("!--- RTK READ FAILURE ---!")
+                        print("!--- READ FAILURE ---!")
         except:
             print("Failed to find the USB for the GPS.")
     
