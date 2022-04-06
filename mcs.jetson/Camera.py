5c7ed054b5beec4b5c7c16b6c63b7ed593069333
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-06 01:45:28 -0400

    Update Camera.py

--- mcs.jetson/Camera.py
+++ mcs.jetson/Camera.py
@@ -7,7 +7,7 @@ import serial
 uart = serial.Serial("/dev/ttyTHS1", baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
 	stopbits=serial.STOPBITS_ONE, timeout=10.00)
 
-net = jetson.inference.imageNet(argv=['--model=~/jetson-inference/python/training/classification/models/blocked/resnet.18.onnx', '--labels=~/jetson-inference/python/training/classification/data/Model1_BlockedPath/labels.txt'])
+net = jetson.inference.imageNet(argv=['--model=~/jetson-inference/python/training/classification/models/blocked/resnet18.onnx', '--labels=~/jetson-inference/python/training/classification/data/Model1_BlockedPath/labels.txt'])
 input = jetson.utils.videoSource("/dev/video0")
 output = jetson.utils.videoOutput("display://0:")
 font = jetson.utils.cudaFont()
