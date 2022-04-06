0a65a9e97dcebe901e5e6740e3fbf7e7d5118abb
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-06 00:15:36 -0400

    Created a file setup the module

---
+++ mcs.jetson/Setup.py
@@ -0,0 +1,14 @@
+import os
+
+if not os.path.isdir("~/jetson-inference"):
+    os.system("git clone --recursive https://github.com/dusty-nv/jetson-inference ~/")
+
+os.system("sudo mkdir ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
+os.system("sudo cp labels.txt ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
+
+os.system("sudo mkdir ~/jetson-inference/python/training/classification/models/blocked/")
+os.system("cp resnet18.onnx ~/jetson-inference/python/training/classification/models/blocked/")
+os.system("cp Camera.py ~/jetson-inference/python")
+
+os.system("cd ~/jetson-inference")
+os.system("sudo docker/run.sh")
