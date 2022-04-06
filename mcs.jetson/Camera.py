b4d0999dde894cf97b3ecf5afec5e00529b5274f
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-05 23:33:28 -0400

    Add Erwin's model and code
    Co-Authored-By: Erwin Castro <90945345+ecastro159@users.noreply.github.com>

---
+++ mcs.jetson/Camera.py
@@ -0,0 +1,44 @@
+import jetson.inference
+import jetson.utils
+import pyserial
+#For more information on the imageNet API:
+#https://rawgit.com/dusty-nv/jetson-inference/python/docs/html/python/jetson.inference.html#imageNet
+
+uart = serial.Serial("dev/tty50", baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE, timeout=10.00)
+
+net = jetson.inference.imageNet('--model=models/blocked/resnet.18.onnx --lables=data/Model1_BlockedPath/lables.txt')
+input = jetson.utils.videoSource("/dev/video0")
+output = jetson.utils.videoOutput("display://0:")
+font = jetson.utils.cudaFont()
+
+while True:
+	# capture the next image
+	img = input.Capture()
+
+	# classify the image
+	class_id, confidence = net.Classify(img)
+
+	# find the object description
+	class_desc = net.GetClassDesc(class_id)
+
+	# Needed only if wanting to display live video feed
+	# # overlay the result on the image	
+	# font.OverlayText(img, img.width, img.height, "{:05.2f}% {:s}".format(confidence * 100, class_desc), 5, 5, font.White, font.Gray40)
+	
+	# # render the image
+	# output.Render(img)
+
+	# # update the title bar
+	# output.SetStatus("{:s} | Network {:.0f} FPS".format(net.GetNetworkName(), net.GetNetworkFPS()))
+
+	# # print out performance info
+	# net.PrintProfilerTimes()
+	message = class_desc
+	uart.write(message.encode())
+
+	# exit on input/output EOS
+	if not input.IsStreaming() or not output.IsStreaming():
+		break
+
+
