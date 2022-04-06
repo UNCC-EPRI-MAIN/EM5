32c0bef0f1f94c80bb02b40e2527dd724f7c867c
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-06 00:55:45 -0400

    Update Setup.py

--- mcs.jetson/Setup.py
+++ mcs.jetson/Setup.py
@@ -10,5 +10,10 @@ os.system("sudo cp -f resnet18.onnx ~/jetson-inference/python/training/classific
 os.system("sudo cp -f Camera.py ~/jetson-inference/python/training/classification/")
 os.system("sudo cp -f run.sh ~/jetson-inference/docker/")
 
+print("")
+print("")
 print("To run the container, you must run the command 'cd ~/jetson-inference' and then 'docker/run.sh'")
-print("In the container, run the python script at 'python/training/classification/Camera.py'")
+print("In the container, first install serial with the command 'pip3 install serial'")
+print("To run the model, run the python script at 'python/training/classification/Camera.py' in the container")
+print("")
+print("")
