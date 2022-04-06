f567ae14cf63fe6bf3bf1595fc63c68bc3b33661
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-06 00:43:23 -0400

    added force commands

--- mcs.jetson/Setup.py
+++ mcs.jetson/Setup.py
@@ -3,11 +3,11 @@ import os
 os.system("git clone --recursive https://github.com/dusty-nv/jetson-inference ~/jetson-inference")
 
 os.system("sudo mkdir ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
-os.system("sudo cp labels.txt ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
+os.system("sudo cp -f labels.txt ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
 
 os.system("sudo mkdir ~/jetson-inference/python/training/classification/models/blocked/")
-os.system("sudo cp resnet18.onnx ~/jetson-inference/python/training/classification/models/blocked/")
-os.system("sudo cp Camera.py ~/jetson-inference/python/training/classification/")
+os.system("sudo cp -f resnet18.onnx ~/jetson-inference/python/training/classification/models/blocked/")
+os.system("sudo cp -f Camera.py ~/jetson-inference/python/training/classification/")
 os.system("sudo cp -f run.sh ~/jetson-inference/docker/")
 
 print("To run the container, you must run the command 'cd ~/jetson-inference' and then 'docker/run.sh'")
