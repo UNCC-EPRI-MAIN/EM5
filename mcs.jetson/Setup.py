1f4dff2d81f0f5e778b389656e8802317820a184
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-06 00:21:25 -0400

    Update Setup.py

--- mcs.jetson/Setup.py
+++ mcs.jetson/Setup.py
@@ -1,7 +1,7 @@
 import os
 
 if not os.path.isdir("~/jetson-inference"):
-    os.system("git clone --recursive https://github.com/dusty-nv/jetson-inference ~/")
+    os.system("git clone --recursive https://github.com/dusty-nv/jetson-inference ~/jetson-inference")
 
 os.system("sudo mkdir ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
 os.system("sudo cp labels.txt ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
@@ -9,3 +9,5 @@ os.system("sudo cp labels.txt ~/jetson-inference/python/training/classification/
 os.system("sudo mkdir ~/jetson-inference/python/training/classification/models/blocked/")
 os.system("sudo cp resnet18.onnx ~/jetson-inference/python/training/classification/models/blocked/")
 os.system("sudo cp Camera.py ~/jetson-inference/python")
+
+os.system("~/jetson-inference/docker/run.sh")
