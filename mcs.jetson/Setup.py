a1cbb680472aa0845264fd7ddffb3067897b9d86
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-06 00:40:25 -0400

    Helper text

--- mcs.jetson/Setup.py
+++ mcs.jetson/Setup.py
@@ -1,13 +1,14 @@
 import os
 
-if not os.path.isdir("~/jetson-inference"):
-    os.system("git clone --recursive https://github.com/dusty-nv/jetson-inference ~/jetson-inference")
+os.system("git clone --recursive https://github.com/dusty-nv/jetson-inference ~/jetson-inference")
 
 os.system("sudo mkdir ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
 os.system("sudo cp labels.txt ~/jetson-inference/python/training/classification/data/Model1_BlockedPath/")
 
 os.system("sudo mkdir ~/jetson-inference/python/training/classification/models/blocked/")
 os.system("sudo cp resnet18.onnx ~/jetson-inference/python/training/classification/models/blocked/")
-os.system("sudo cp Camera.py ~/jetson-inference/python/training/classification")
+os.system("sudo cp Camera.py ~/jetson-inference/python/training/classification/")
+os.system("sudo cp -f run.sh ~/jetson-inference/docker/")
 
-os.system("~/jetson-inference/docker/run.sh")
+print("To run the container, you must run the command 'cd ~/jetson-inference' and then 'docker/run.sh'")
+print("In the container, run the python script at 'python/training/classification/Camera.py'")
